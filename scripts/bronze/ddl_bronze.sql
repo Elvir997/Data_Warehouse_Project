3c0bec530a2e2287f05de95016bbdaf32f13d2f0
Elvir997 <elvirfernando997@gmail.com>
2026-04-20 10:00:32 -0600

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -15,7 +15,7 @@ event_id varchar(50),
 user_id varchar(50), 
 product_id varchar(50),
 event_type varchar(50),
-event_time varchar(50));
+event_date_time DATETIME);
 
 drop table if exists bronze_crm_sales;
 create table bronze_crm_sales (
@@ -32,7 +32,7 @@ drop table if exists bronze_crm_orders;
 create table bronze_crm_orders (
 order_id varchar(50),
 user_id varchar(50),
-order_date varchar(50), 
+order_date DATETIME, 
 order_status varchar(50),
 total_amount int
 );
@@ -55,7 +55,7 @@ product_id varchar(50),
 user_id varchar(50),
 rating int,
 review_text varchar(100),
-review_date varchar(50)
+review_date DATETIME
 );
 
 
@@ -66,5 +66,5 @@ complete_name varchar(50),
 gender varchar(50),
 email varchar(50), 
 city varchar(50),
-signup_date varchar(50)
+signup_date DATE
 );
